4362546cf22f9600142c5ca2d0a5477decf904a2
ElizaG00d <gooden.laurel.232@outlook.com>
2022-12-12 10:17:05 -0600

    upgraded portfolio, limited js

---
+++ js/javascript.js
@@ -0,0 +1,14 @@
+
+//pop up form js//
+function formOpen() {
+    document.getElementById('contactForm').style.display = 'block';
+}
+
+function formClose() {
+    document.getElementById('contactForm').style.display = 'none';
+}
+
+//end popup form js//
+
+//clicking sound//
+let clicky = () => new Audio("./audio/../audio/mixit-single-key-press.wav").play();
