ff36f5cc8a50051843b63716ffa943de0d5bf25d
ElizaG00d <gooden.laurel.232@outlook.com>
2023-05-05 21:43:49 -0500

    jquery, removed portfolio, sidebar frame

--- js/javascript.js
+++ js/javascript.js
@@ -2,11 +2,11 @@
 //pop up form js//
 function formOpen() {
     document.getElementById('contactForm').style.display = 'block';
-}
+};
 
 function formClose() {
     document.getElementById('contactForm').style.display = 'none';
-}
+};
 
 //end popup form js//
 
@@ -14,10 +14,10 @@ function formClose() {
 let clicky = () => new Audio("./audio/../audio/mixit-single-key-press.wav").play();
 
 //side panel bit
-function openPanel() {
+function openPane() {
     document.getElementById("sidePanel").style.width = "250px";
-}
+};
 
-function closePanel() {
-    document.getElementById("sidePanel").style.width = "30px";
-}
+function closePane() {
+    document.getElementById("sidePanel").style.width = "27px";
+};
--- js/jquery.js
+++ js/jquery.js
@@ -1,1 +1,35 @@
 //ADVANCED JS ADDITIONS
+
+//onload settings for panels
+$(document).ready(function(){
+    $("div.info").hide();
+    //$("div.panel.side").hide();
+});
+
+//hide and show panels
+$("#about").on("click", function(){
+    $("div.about-txt").slideToggle();
+});
+
+$("#projects").on("click", function(){
+    $("div.projects-txt").slideToggle();
+});
+
+$("#contact").on("click", function(){
+    $("div.form-div").slideToggle();
+});
+
+//side panel toggle
+//$(".panel").on("click", function(){
+    //$(".panel.side").sidebar().trigger("sidebar:open");
+//});
+
+//$(".panel.side").sidebar({
+    //side:"left",
+    //isClosed: false,
+    //close: true
+//});
+
+//$(".panel.side").sidebar().trigger("sidebar:open");
+//$(".panel.side").sidebar().trigger("sidebar:close");
+//$(".panel.side").sidebar().trigger("sidebar:toggle");
